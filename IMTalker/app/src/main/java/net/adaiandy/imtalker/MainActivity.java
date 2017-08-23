5f69c22262df71debd64b444764456b42287f353
pengjintu <pengjintu@yy.com>
2017-08-24 00:38:29 +0800

    【APP】主界面封装 4.1.0

--- IMTalker/app/src/main/java/net/adaiandy/imtalker/MainActivity.java
+++ IMTalker/app/src/main/java/net/adaiandy/imtalker/MainActivity.java
@@ -1,18 +1,21 @@
 package net.adaiandy.imtalker;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
+import android.support.design.widget.BottomNavigationView;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+
 import net.adaiandy.common.app.BaseActivity;
+import net.adaiandy.common.widget.PortraitView;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+
 
 /**
  * @author
@@ -21,28 +24,54 @@ import butterknife.OnClick;
  */
 
 public class MainActivity extends BaseActivity {
-    
-    @BindView(R.id.txt_display)
-     TextView mTextView;
-    
-    @BindView(R.id.edit_content)
-     EditText mEditText;
 
+    @BindView(R.id.appbar)
+    View mLayAppbar;
+
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
 
     @Override
     protected int getContentLayoutId() {
-        return R.layout.layout_main;
+        return R.layout.activity_main;
     }
 
-    @OnClick(R.id.btn_submmit)
-    void onSubmmit(){
-        mTextView.setText(mEditText.getText());
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+
+
+        Glide.with(this).load(R.drawable.bg_src_morning).centerCrop().into(new ViewTarget<View, GlideDrawable>(mLayAppbar) {
+            @Override
+            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                this.view.setBackground(resource.getCurrent());
+            }
+        });
+        
     }
 
     @Override
     protected void initData() {
         super.initData();
-        
+    }
+
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick() {
+
+    }
+
+    @OnClick(R.id.btn_action)
+    void onActionClick() {
+
     }
 }
 
